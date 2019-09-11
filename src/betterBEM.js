8bcd1397e2d3b639b5927c7f65542f0e02f05066
Luud Jacobs <luud.jacobs@pwc.com>
2019-09-11 11:09:39 +0200

    renamed `cssModuleStyle` to `classNameMap` and added option for strict handling

--- src/betterBEM.js
+++ src/betterBEM.js
@@ -3,8 +3,7 @@ import { isString, isPlainObject, isEmptyObject } from 'typechecker';
 const objectKeysToClassNamesReducer = (acc, classNames) => {
     if (isPlainObject(classNames)) {
         // return object keys for which value is thruthy
-        const enabledClassNames = Object.keys(classNames)
-        .filter((key) => classNames[key]);
+        const enabledClassNames = Object.keys(classNames).filter((key) => classNames[key]);
 
         return [ ...acc, ...enabledClassNames ];
     }
@@ -15,53 +14,72 @@ const generateClassNamesArray = (input = []) => (
     [input].flat()
         .reduce(objectKeysToClassNamesReducer, [])
         .reduce((acc, className) => (
-            isString(className) ? [ ...acc, ...className.split(/\s+/g) ] : acc
+            isString(className) ? [
+                ...acc,
+                ...className.split(/\s+/g).filter((str) => str)
+            ] : acc
         ), [])
 );
 
 // not perfect yet
-const combineClassNames = (baseClassNames, extraClassNames, concat = '__') => (
-    baseClassNames.reduce((acc, baseCn = '') => ([
+const combineClassNames = (baseClassNames, extraClassNames, concat = '__') => {
+    if (!baseClassNames.length) {
+        return extraClassNames;
+    }
+    if (!extraClassNames.length) {
+        return baseClassNames;
+    }
+    return baseClassNames.reduce((acc, baseCn = '') => ([
         ...acc,
         ...extraClassNames.map((extraCn = '') => `${baseCn}${baseCn && extraCn ? concat : ''}${extraCn}`)
     ]), [])
-);
+};
 
 /**
  * BEM classname generator.
  * Creates function which returns a chainable BEM object.
  *
- * @param {string|array}    baseClassName    base classname for block element
- * @param {Object}          cssModuleStyle   CSS modules style object
+ * @param {string|array}    baseClassName       base classname for block element
+ * @param {Object}          classNameMap        CSS modules style object
+ * @param {Boolean}         strictClassNameMap  if true drops classNames which don't appear in classNameMap
  *
  * @return {Object} BEM object
  */
-const BEM = (baseClassName = [], cssModuleStyle = {}) => {
+const BEM = (baseClassName = [], classNameMap = {}, strictClassNameMap = true) => {
     const baseClassNames = generateClassNamesArray(baseClassName);
 
     return {
         get cn() {
-            if (isPlainObject(cssModuleStyle) && !isEmptyObject(cssModuleStyle)) {
-                const filteredClassNames = baseClassNames
-                    .map((className) => cssModuleStyle[className])
-                    .filter((moduleClassName) => moduleClassName);
+            if (isPlainObject(classNameMap) && !isEmptyObject(classNameMap)) {
+                let filteredClassNames;
+                switch (strictClassNameMap) {
+                    case false: {
+                        filteredClassNames = baseClassNames
+                            .map((className) => classNameMap[className] || className);
+                        break;
+                    }
+                    // case true:
+                    default: {
+                        filteredClassNames = baseClassNames
+                            .map((className) => classNameMap[className])
+                            .filter((moduleClassName) => moduleClassName);
+                    }
+                }
                 return filteredClassNames.join(' ');
             }
-            baseClassNames.join(' ');
+
+            return baseClassNames.join(' ');
         },
-        el: (elementClassName) => {
+        el: (elementClassName = []) => {
             const elementClassNames = generateClassNamesArray(elementClassName);
             const allClassNames = combineClassNames(baseClassNames, elementClassNames);
-            return BEM(allClassNames, cssModuleStyle);
+            return BEM(allClassNames, classNameMap, strictClassNameMap);
         },
-        mod: (modifiers) => {
-            console.log({modifiers});
+        mod: (modifiers = []) => {
             const modifierClassNames = generateClassNamesArray(modifiers);
-            console.log({modifierClassNames});
-            console.log({baseClassName});
-            const allClassNames = combineClassNames(baseClassNames, modifierClassNames, '--');
-            console.log({allClassNames});
-            return BEM(allClassNames, cssModuleStyle);
+            const allModifiedClassNames = combineClassNames(baseClassNames, modifierClassNames, '--');
+            const allClassNames = [ ...baseClassNames, ...allModifiedClassNames ];
+            return BEM(allClassNames, classNameMap, strictClassNameMap);
         }
     };
 };
